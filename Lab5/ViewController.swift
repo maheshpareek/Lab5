dc9dfb5ba7fe9069145a6e6bcf8f26aee566aa7b
Mahesh Pareek <pareekm644@gmail.com>
2024-02-08 14:53:26 -0500

    Initial Commit

---
+++ Lab5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab5
+//
+//  Created by Mahesh Pareek on 2024-02-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
